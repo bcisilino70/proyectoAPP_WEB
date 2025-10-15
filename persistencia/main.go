15299cfd14a80b65be3c37ced05a6dc4de03b4c8
TomasDiCarlo <tdicarlo@alumnos.exa.unicen.edu.ar>
2025-10-15 12:06:22 -0300

    agregado de dos funciones en main.go queda probarlo

--- persistencia/main.go
+++ persistencia/main.go
@@ -21,7 +21,9 @@ func main() {
 	queries := sqlc.New(db)
 	ctx := context.Background()
 
-	// --- CREAR USUARIO
+	// --- OPERACIONES CRUD SOBRE CLIENTE --- //
+
+	// --- CREAR USUARIO -- CREATE
 	crearUsuario, err := queries.CreateCliente(ctx, // Create
 		sqlc.CreateClienteParams{
 			Nombre:   "Tomas",
@@ -35,14 +37,14 @@ func main() {
 	}
 	fmt.Printf("Creado correctamente: %+v\n", crearUsuario)
 
-	// --- GET CLIENTE
+	// --- GET CLIENTE -- READ
 	usuario, err := queries.GetCliente(ctx, crearUsuario.ID) // Read One
 	if err != nil {
 		log.Fatalf("Error al buscar usuario: %v", err)
 	}
 	fmt.Printf("Usuario encontrado: %+v\n", usuario)
 
-	// --- UPDATE CLIENTE
+	// --- UPDATE CLIENTE -- UPDATE
 	err = queries.UpdateCliente(ctx, sqlc.UpdateClienteParams{ // Update
 		Email:   "johnny.doe@example.com",
 		Usuario: crearUsuario.Usuario,
@@ -52,7 +54,16 @@ func main() {
 		log.Fatalf("Error al modificar cliente: %v", err)
 	}
 
-	// --- CREAR RESENA
+	// --- DELETE CLIENTE -- DELETE
+	err = queries.DeleteCliente(ctx, crearUsuario.Usuario)
+	if err != nil {
+		log.Fatalf("Error al eliminar cliente: %v", err)
+	}
+	fmt.Println("Cliente eliminado correctamente")
+
+	// --- OPERACIONES CRUD SOBRE CLIENTE --- //
+
+	// --- CREAR RESENA -- CREATE
 	crearResena, err := queries.CreateResena(ctx,
 		sqlc.CreateResenaParams{
 			Titulo:      "Centinela",
@@ -65,11 +76,28 @@ func main() {
 	}
 	fmt.Printf("Creado correctamente: %+v\n", crearResena)
 
-	// --- GET RESENAS
+	// --- GET RESENAS --
 	resenas, err := queries.ListResenas(ctx, crearUsuario.ID)
 	if err != nil {
 		log.Fatalf("Error al crear resena: %v", err)
 	}
 	fmt.Printf("Resenas de un usuario: %+v\n", resenas)
+
+	// --- UPDATE RESENA -- UPDATE
+	err = queries.UpdateResena(ctx, sqlc.UpdateResenaParams{
+		Titulo:      "Centinela",
+		Descripcion: "El mejor lugar de tandil",
+	})
+	if err != nil {
+		log.Fatalf("Error al modificar resena: %v", err)
+	}
+	fmt.Println("Resena modificada correctamente")
+
+	// --- DELETE RESENA -- DELETE
+	err = queries.DeleteResena(ctx, crearResena.Titulo)
+	if err != nil {
+		log.Fatalf("Error al eliminar resena: %v", err)
+	}
+	fmt.Println("Resena eliminada correctamente")
 
 }
